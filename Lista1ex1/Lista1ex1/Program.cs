4cf8e8e2582b4a7b454fa26ac6df0fedce6813b1
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-01 21:13:48 -0300

    Add files via upload

---
+++ Lista1ex1/Lista1ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+            string aux;
+
+            Console.Write("Digite a base do retângulo: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            a = int.Parse(Console.ReadLine());
+
+            area = a * b;
+
+            Console.Write("A área do retângulo é: ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
