efa5b86eacd742f5a809ab266ea99a464749f4fb
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-15 20:33:55 -0300

    Add files via upload

---
+++ Lista1ex2/Lista1ex2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int l;
+            int area;
+
+            Console.WriteLine("Calcular e exibir a área de um quadrado");
+            Console.WriteLine("Digite o valor da aresta do quadrado: ");
+            l = int.Parse(Console.ReadLine());
+
+            area = l * l;
+
+            Console.WriteLine("O valor da área do quadrado é: ");
+            Console.WriteLine(area);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
