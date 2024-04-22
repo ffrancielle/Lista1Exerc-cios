0b1ffb33150fab3961eeda5f21ad5b490de45339
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-22 20:06:39 -0300

    Add files via upload

---
+++ Lista1ex4/Lista1ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+            Console.Write("Digite a base do triângulo: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do triângulo: ");
+            a = int.Parse(Console.ReadLine());
+
+            area = (a * b) / 2;
+
+            Console.Write("A área do triângulo é: ");
+            Console.WriteLine(area);
+        }
+    }
+}
