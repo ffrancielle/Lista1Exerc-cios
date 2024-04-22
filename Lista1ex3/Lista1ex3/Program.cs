3df5ef434cb5a1bce8a093747154643b34d5f2a5
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-22 19:49:21 -0300

    Add files via upload

---
+++ Lista1ex3/Lista1ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal;
+            double area;
+
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            diagonal = double.Parse(Console.ReadLine());
+
+            area = Math.Pow(diagonal, 2) / 2;
+
+            Console.WriteLine("A área do quadrado é: ");
+            Console.WriteLine(area);
+
+
+
+        }
+    }
+}
