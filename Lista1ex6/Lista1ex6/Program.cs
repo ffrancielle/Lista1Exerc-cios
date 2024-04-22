90526f8a56be9ad99724678f7a2248f8567d609c
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-22 20:41:42 -0300

    Add files via upload

---
+++ Lista1ex6/Lista1ex6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1;
+            double v2;
+            double media;
+
+            Console.Write("Digite o primeiro valor: ");
+            v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            v2 = double.Parse(Console.ReadLine());
+
+            media = Math.Sqrt(v1 * v2);
+
+            Console.WriteLine("O valor da média geométrica é: ");
+            Console.WriteLine(media);
+
+        }
+    }
+}
