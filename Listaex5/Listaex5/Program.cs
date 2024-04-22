c89a7309511e6499a1720d5d8eef71c5475cdd0e
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-22 20:24:42 -0300

    Add files via upload

---
+++ Listaex5/Listaex5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listaex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1;
+            double v2;
+            double v3;
+            double v4;
+            double media;
+
+            Console.Write("Digite o primeiro valor: ");
+            v1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            v2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o terceiro valor: ");
+            v3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o quarto valor: ");
+            v4 = double.Parse(Console.ReadLine());
+
+            media = (v1 + v2 + v3 + v4) / 4;
+
+            Console.WriteLine("O valor da média aritmética: ");
+            Console.WriteLine(media);
+
+
+
+
+
+        }
+    }
+}
