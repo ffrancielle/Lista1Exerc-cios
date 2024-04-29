9cc56130dd14be145b353b6bcff9b76b1a9a9355
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-29 12:07:10 -0300

    Add files via upload

---
+++ Lista1ex10/Lista1ex10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Conversor de Dólares para Reais");
+
+            Console.Write("Digite a cotação do dólar (em Reais): ");
+            double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de dólares: ");
+            double quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+
+            double valorEmReais = cotacaoDolar * quantidadeDolares;
+
+            Console.WriteLine("O valor de " + quantidadeDolares.ToString() + " dólares equivale a R$" + valorEmReais.ToString() + ".");
+
+
+
+
+
+        }
+    }
+}
