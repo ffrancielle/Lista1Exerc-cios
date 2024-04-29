f2be51f3bcaecce25904ee8520ce8f2e04345206
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-29 11:44:03 -0300

    Add files via upload

---
+++ Lista1ex9/Lista1ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de Área de Círculo");
+
+            Console.Write("Digite o diâmetro do círculo: ");
+            double diametro = Convert.ToDouble(Console.ReadLine());
+
+            double raio = diametro / 2;
+
+            double area = Math.PI * Math.Pow(raio, 2);
+
+            Console.WriteLine("A área do círculo é: " + area.ToString());
+        }
+    }
+}
