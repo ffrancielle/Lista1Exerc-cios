7030b34c5bdcdc4eea48eb81da25b7bc3f69d732
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-29 16:01:29 -0300

    Add files via upload

---
+++ Lista1ex12/Lista1ex12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de 5 produtos:");
+
+            double total = 0;
+
+            for (int i = 1; i <= 5; i++)
+
+            Console.Write("Produto " + i + ": ");
+            total += Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o valor do pagamento: ");
+            double pagamento = Convert.ToDouble(Console.ReadLine());
+
+            double troco = pagamento - total;
+
+            Console.WriteLine("Troco: " + troco);
+
+
+        }
+    }
+}
