d9ca383549b3a5e8d8b8ea54c9d6e030c15c957c
ffrancielle <165734193+ffrancielle@users.noreply.github.com>
2024-04-29 11:08:39 -0300

    Add files via upload

---
+++ Lista1ex7/Lista1ex7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de Milhas Marítimas para Quilômetros");
+
+            double milhasMetros = 1852;
+            double quilometrosMetros = 1000;
+
+            Console.Write("Digite a o valor de milhas marítimas: ");
+            double milhas = Convert.ToDouble(Console.ReadLine());
+
+            double metros = milhas * milhasMetros;
+
+            double quilometros = metros / quilometrosMetros;
+
+            Console.WriteLine(milhas + " milhas marítimas equivalem a " + quilometros + " quilômetros.");
+
+
+
+
+
+
+        }
+    }
+}
